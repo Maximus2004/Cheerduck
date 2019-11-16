e4ab87e41ae2dfb82e927ba1f4aa704fa3f3b6aa
Maximus2004 <maks09062004@mail.ru>
2019-11-16 18:41:55 +0300

    Shabloni

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from blog.models import Post
+import blog.views
+import blog.models
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})

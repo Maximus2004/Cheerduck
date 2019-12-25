6bbe3e7674c5b20f01ccd30c0a4ca2d7b77ee49a
Maximus2004 <maks09062004@mail.ru>
2019-12-25 22:19:41 +0300

    красивый шаблон под регистрацию

--- blog/forms.py
+++ blog/forms.py
@@ -9,27 +9,27 @@ from django.contrib.auth.models import User
 
 
 class SignUpForm(UserCreationForm):
-  email = forms.EmailField(max_length=254, help_text='Это поле обязательно')
-  Class = forms.CharField(max_length=2, help_text='Впишите сюда класс, в котором вы обучаетесь')
-
-  class Meta:
-    model = User
-    fields = ('username', 'email', 'password1', 'password2', 'first_name', 'last_name', 'Class')
+    email = forms.EmailField(max_length=254)
+    Class = forms.CharField(max_length=2)
+    help_texts = {
+        'email': ' ',
+    }
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', 'first_name', 'last_name', 'Class')
 
 class RegisterFormView(FormView):
     form_class = SignUpForm
-
     # Ссылка, на которую будет перенаправляться пользователь в случае успешной регистрации.
     # В данном случае указана ссылка на страницу входа для зарегистрированных пользователей.
-    success_url = "/login/"
+    success_url = '/'
 
     # Шаблон, который будет использоваться при отображении представления.
-    template_name = "blog/new_user.html"
+    template_name = "blog/signup.html"
 
     def form_valid(self, form):
         # Создаём пользователя, если данные в форму были введены корректно.
         form.save()
-
         # Вызываем метод базового класса
         return super(RegisterFormView, self).form_valid(form)
 
